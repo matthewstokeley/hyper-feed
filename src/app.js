d28d405dddccbdac6604b10811174f3762c7e84c
mstokeley <matthewjaestokeley@gmail.com>
2020-02-13 13:01:40 -0500

    export variables

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const html = hyperx(hyperapp.h)
+const model = 0
+
+export var click = model => model + 1
+
+export var doubleClick = ( model, data, actions ) => {
+  return actions.click( actions.click( model ) )
+}
+
+const action = { click, doubleClick }
+
+const view = ( model, actions ) => {
+
+  return html`
+    <div>
+      <h1>Clicked ${model}</h1>
+      <button onClick=${actions.click}>click</button>
+    </div>
+  `
+}
+
+hyperapp.app({
+  model,
+  action,
+  view
+})
